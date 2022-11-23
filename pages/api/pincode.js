2959b78a1e17d3f911c6ee7418069c2306669c07
SmitHacker <102592902+SmitDasadia@users.noreply.github.com>
2022-11-23 20:20:58 +0530

    Add Pincode Serviceability Check

---
+++ pages/api/pincode.js
@@ -0,0 +1,6 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+    res.status(200).json([360001,360002,36003,360004,360005])
+  }
+  
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -5,8 +5,25 @@ import { useRouter } from 'next/router'
 const Slug = () => {
   const router = useRouter()
   const { slug } = router.query
+
   const [show, setShow] = useState(false);
   const [show2, setShow2] = useState(false);
+  const [pin, setPin] = useState()
+  const [service, setService] = useState()
+  const checkPincode = async () => {
+    const pins = await fetch('http://localhost:3000/api/pincode')
+    let pinJson = await pins.json()
+    if (pinJson.includes(parseInt(pin))){
+      setService(true)
+    }
+    else {
+      setService(false)
+    }
+  }
+
+  const onChangePinCode = (e) => {
+    setPin(e.target.value)
+  }
   return (
     <>
       <section class="pt-12 pb-24 bg-blueGray-100 rounded-b-10xl overflow-hidden">
@@ -123,8 +140,9 @@ const Slug = () => {
                   <div class="w-6 h-6 rounded-full bg-yellow-500"></div>
                 </button>
               </div>
-              <div class="mb-10">
-                <h4 class="mb-3 font-heading font-medium">Qty:</h4>
+
+              <div class="Size mb-10">
+                <h4 class="mb-3 font-heading font-medium">Size:</h4>
                 <select class="w-24 px-3 py-2 text-center bg-white border-2 border-blue-500 outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-xl">
                   <option>S</option>
                   <option>M</option>
@@ -137,12 +155,20 @@ const Slug = () => {
                   <option>6XL</option>
                 </select>
                 <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4" viewBox="0 0 24 24">
-                  <path d="M6 9l6 6 6-6"></path>
-                </svg>
-              </span>
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokewidth="2" class="w-4 h-4" viewBox="0 0 24 24">
+                    <path d="M6 9l6 6 6-6"></path>
+                  </svg>
+                </span>
+              </div>
 
-                {/* <input class="w-24 px-3 py-2 text-center bg-white border-2 border-blue-500 outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-xl" type="text" placeholder="1" /> */}
+              <div class="pincode mb-10">
+                <h4 class="mb-3 font-heading font-medium">Pincode:</h4>
+                <input onChange={onChangePinCode} class=" px-3 py-2 text-center bg-white border-2 border-blue-500 outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-xl" type="text" placeholder="Enter Your Pincode" /> <button
+                  class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl" onClick={checkPincode}>
+                  Check
+                </button>
+                {service && service != null && <div class="text-green-800">This Pincode is Servivceable!</div>}
+                {!service && service != null && <div class="text-red-800">Sorry! This Pincode is not Servivceable!</div>}
               </div>
               <div class="flex flex-wrap -mx-2 mb-12">
                 <div class="w-full md:w-2/3 py-3 px-2 mb-2 md:mb-0"><a class="block py-4 px-2 leading-8 font-heading font-medium tracking-tighter text-xl text-white text-center bg-blue-500 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 hover:bg-blue-600 rounded-xl" href="#">Buy Now</a></div>
@@ -151,7 +177,7 @@ const Slug = () => {
                   <a class="flex w-full py-4 px-2 items-center justify-center leading-8 font-heading font-medium tracking-tighter text-xl text-center bg-white focus:ring-2 focus:ring-gray-200 focus:ring-opacity-50 hover:bg-opacity-60 rounded-xl" href="#">
                     <span class="mr-2">Wishlist</span>
                     <svg width="23" height="22" viewbox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M11.3235 20.1324L2.52488 10.8515C1.75232 10.0706 1.24237 9.06367 1.06728 7.97339C0.8922 6.88311 1.06086 5.76477 1.54936 4.7768V4.7768C1.91837 4.03089 2.45739 3.3843 3.12201 2.89033C3.78663 2.39635 4.55781 2.06911 5.37203 1.93558C6.18626 1.80205 7.0202 1.86605 7.80517 2.1223C8.59013 2.37855 9.30364 2.81972 9.88691 3.40946L11.3235 4.86204L12.7601 3.40946C13.3434 2.81972 14.0569 2.37855 14.8419 2.1223C15.6269 1.86605 16.4608 1.80205 17.275 1.93558C18.0892 2.06911 18.8604 2.39635 19.525 2.89033C20.1897 3.3843 20.7287 4.03089 21.0977 4.7768V4.7768C21.5862 5.76477 21.7549 6.88311 21.5798 7.97339C21.4047 9.06367 20.8947 10.0706 20.1222 10.8515L11.3235 20.1324Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      <path d="M11.3235 20.1324L2.52488 10.8515C1.75232 10.0706 1.24237 9.06367 1.06728 7.97339C0.8922 6.88311 1.06086 5.76477 1.54936 4.7768V4.7768C1.91837 4.03089 2.45739 3.3843 3.12201 2.89033C3.78663 2.39635 4.55781 2.06911 5.37203 1.93558C6.18626 1.80205 7.0202 1.86605 7.80517 2.1223C8.59013 2.37855 9.30364 2.81972 9.88691 3.40946L11.3235 4.86204L12.7601 3.40946C13.3434 2.81972 14.0569 2.37855 14.8419 2.1223C15.6269 1.86605 16.4608 1.80205 17.275 1.93558C18.0892 2.06911 18.8604 2.39635 19.525 2.89033C20.1897 3.3843 20.7287 4.03089 21.0977 4.7768V4.7768C21.5862 5.76477 21.7549 6.88311 21.5798 7.97339C21.4047 9.06367 20.8947 10.0706 20.1222 10.8515L11.3235 20.1324Z" stroke="black" strokewidth="2" stroke-linecap="round" stroke-linejoin="round"></path>
                     </svg>
                   </a>
                 </div> */}
