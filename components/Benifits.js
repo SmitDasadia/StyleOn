1c8f445121252146477ba6acf7b1af9c492f96ed
SmitHacker <102592902+SmitDasadia@users.noreply.github.com>
2023-01-20 19:58:17 +0530

    remove letter

--- components/Benifits.js
+++ components/Benifits.js
@@ -20,15 +20,16 @@ const Benifits = () => {
     },
   ]
 
+
   return (
     <div>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
-         s
+       
           <div className="flex flex-wrap -m-4 text-center">
             {benifits.map((benifits) => (
               <div key={benifits.name} className="p-4 md:w-1/4 sm:w-1/2 w-full">
-                <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                <div className="px-4 py-6 rounded-lg">
                   <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
                     <path d="M8 17l4 4 4-4m-4-5v9"></path>
                     <path d="M20.88 18.09A5 5 0 0018 9h-1.26A8 8 0 103 16.29"></path>
